f1330dd715be91fb60a48a24bea0908dcf74790e
DGlovecraft <nuttawutexp@gmail.com>
2024-11-26 11:12:16 +0700

    Updatevscode

---
+++ TestoopXD/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string name = "Nattawut Chansanit";
+string id_ST = "673450036-4";
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is " + name);
+Console.WriteLine("My id " + id_ST);
+
+
